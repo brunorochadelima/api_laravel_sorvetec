6acc9d32b740729f59accfdfc2a1a29b58d069fe
Bruno Rocha de Lima <brunorocha33@live.com>
2022-09-08 10:07:49 -0300

    aurtenticando rotas

--- routes/api.php
+++ routes/api.php
@@ -7,30 +7,46 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostsController;
 use App\Http\Controllers\CategoriesController;
 use App\Models\Category;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Middleware\Authenticate;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+/* |-------------------------------------------------------------------------- | API Routes |-------------------------------------------------------------------------- | | Here is where you can register API routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | is assigned the "api" middleware group. Enjoy building your API! | */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/posts', [PostsController::class, 'index']);
-Route::get('/posts/{id}', [PostsController::class, 'show']);
-Route::post('/posts', [PostsController::class, 'store']);
-Route::delete('/posts/{id}', [PostsController::class, 'destroy']);
-Route::put('/posts/{id}', [PostsController::class, 'update']);
+Route::post('/login', function (Request $request) {
+    $credenciais = $request->only(['email', 'password']);
+    if (Auth::attempt($credenciais) === false) {
+        return response()->json('Unauthorized', 401);
+    }
+    ;
+    $user = Auth::user();
+    $token = $user->createToken('token');
+    return response()->json($token);
+});
 
-Route::get('/categories', [CategoriesController::class, 'index']);
-Route::get('/categories/{id}/posts',[PostsController::class, 'show_post_by_category']);
-Route::post('/categories', [CategoriesController::class, 'store']);
-Route::delete('/categories/{id}', [CategoriesController::class, 'destroy']);
-Route::put('/categories/{id}', [CategoriesController::class, 'update']);
+//Rotas autenticadas
+
+Route::middleware('auth:sanctum')->group(function () {
+    //posts
+    Route::post('/posts', [PostsController::class , 'store']);
+    Route::delete('/posts/{id}', [PostsController::class , 'destroy']);
+    Route::put('/posts/{id}', [PostsController::class , 'update']);
+
+    //categorias
+    Route::post('/categories', [CategoriesController::class , 'store']);
+    Route::delete('/categories/{id}', [CategoriesController::class , 'destroy']);
+    Route::put('/categories/{id}', [CategoriesController::class , 'update']);
+});
+
+//Rotas Públicas
+
+//Posts
+Route::get('/posts', [PostsController::class , 'index']);
+Route::get('/posts/{id}', [PostsController::class , 'show']);
+
+//Categorias
+Route::get('/categories', [CategoriesController::class , 'index']);
+Route::get('/categories/{id}/posts', [PostsController::class , 'show_post_by_category']);
